20f751bd282a99ffe2b89874ccfda0700ac6ee52
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-12-06 17:11:03 +0100

    Integrate aiUseChatAdapter for chat response handling and enable streaming

--- app/api/chat-stream/route.ts
+++ app/api/chat-stream/route.ts
@@ -1,4 +1,5 @@
 import { ragChat } from "@/app/lib/rag-chat";
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
@@ -6,10 +7,9 @@ export const POST = async (req: NextRequest) => {
       const { messages, sessionId } = await req.json();
       const lastMessage = messages[messages.length - 1].content;
   
-      const response = await ragChat.chat(lastMessage);
-      console.log("response", response);
+      const response = await ragChat.chat(lastMessage, {streaming: true, sessionId});
   
-      return NextResponse.json(response);
+      return aiUseChatAdapter(response);
     } catch (error) {
       console.error("Error:", error);
       return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
