c33984e50e6477caf53af28d669ec28218c04552
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-12-18 14:44:03 +0100

    Add session management and batch URL indexing to chat page

--- app/[...url]/page.tsx
+++ app/[...url]/page.tsx
@@ -1,6 +1,7 @@
 import ChatWrapper from "../components/chatWrapper";
 import { ragChat } from "../../lib/rag-chat";
 import { redis } from "../../lib/redis";
+import { cookies } from "next/headers";
 
 interface PageProps {
   params: {
@@ -16,27 +17,46 @@ function reconstructUrl({ url }: { url: string[] }) {
 }
 
 const Page = async ({ params }: PageProps) => {
+  const sessionCookie = (await cookies()).get("sessionId")?.value;
   const { url } = await params;
   const recounstructedUrl = reconstructUrl({ url: url as string[] });
-  console.log("recounstructedUrl", recounstructedUrl);
+  const sessionId = (recounstructedUrl + "--" + sessionCookie).replace(
+    /\//g,
+    ""
+  );
+
   const isAlreadyIndexed = await redis.sismember(
     "indexed-urls",
     recounstructedUrl
   );
 
-  const sessionId = "mock-session-id";
+  const initialMessages = await ragChat.history.getMessages({
+    amount: 10,
+    sessionId: sessionId,
+  });
 
   console.log("isAlreadyIndexed", isAlreadyIndexed);
+  async function addUrlsInBatches(urls: string[], batchSize: number) {
+    for (let i = 0; i < urls.length; i += batchSize) {
+      const batch = urls.slice(i, i + batchSize);
+      await redis.sadd("indexed-urls", ...(batch as [string, ...string[]]));
+    }
+  }
 
   if (isAlreadyIndexed === 0) {
     await ragChat.context.add({
       type: "html",
       source: recounstructedUrl,
-      config: { chunkOverlap: 50, chunkSize: 200 },
+      config: { chunkOverlap: 50, chunkSize: 500 },
     });
-    await redis.sadd("indexed-urls", recounstructedUrl);
+
+    const urlsToAdd = [recounstructedUrl]; // Lägg till fler URL:er om det behövs
+    const batchSize = 1000; // Max batchstorlek
+    await addUrlsInBatches(urlsToAdd, batchSize);
   }
 
-  return <ChatWrapper sessionId={sessionId} />;
+  return (
+    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
+  );
 };
 export default Page;
