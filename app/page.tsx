2ea1969fffb79a6e7b5144c8fba1ae80b52a0357
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-12-19 17:12:02 +0100

    Refactor imports in page.tsx to streamline component usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-import { Button } from "@nextui-org/react";
-import {
-  AppWindowIcon,
-  BotMessageSquare,
-  LayoutTemplate,
-  MessageSquare,
-  MessageSquareCode,
-} from "lucide-react";
+import { AppWindowIcon, BotMessageSquare, MessageSquare } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
