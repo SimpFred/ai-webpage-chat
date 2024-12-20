3ad1472c01ae2c7ff5183e058b09a662c5f53525
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-12-20 14:33:11 +0100

    Enhance styling and layout of links in Home component for improved UI

--- app/page.tsx
+++ app/page.tsx
@@ -9,28 +9,26 @@ export default function Home() {
           <h1 className="text-4xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600">
             Välkommen till detta <br /> LLM Interface! 🤖
           </h1>
-          <div className="inline-flex gap-4">
+          <div className="inline-flex gap-4 w-full">
             <Link
               href="/web-page-chat"
-              className="w-fit mt-6 z-10 bg-zinc-900 flex flex-col border-2 border-border dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 rounded-lg p-2 items-center justify-center mx-auto"
+              className="flex-1 mt-6 z-10 bg-gradient-to-r from-blue-500 to-purple-500 flex flex-col rounded-lg p-2 items-center justify-center hover:shadow-lg hover:from-blue-600 hover:to-purple-600 transition-all duration-300"
             >
               <div className="flex">
-                <AppWindowIcon size={30} className="m-1 text-blue-500" />
-                <MessageSquare size={30} className="m-1 text-blue-500" />
+                <AppWindowIcon size={30} className="m-1 text-white" />
+                <MessageSquare size={30} className="m-1 text-white" />
               </div>
-
-              <p className=" text-xs">Chatta med en hemsida</p>
+              <p className="text-xs text-white">Chatta med en hemsida</p>
             </Link>
             <Link
               href="/chat"
-              className="w-fit mt-6 z-10 bg-zinc-900 flex flex-col border-2 border-border dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 rounded-lg p-2 items-center justify-center mx-auto"
+              className="flex-1 mt-6 z-10 bg-gradient-to-r from-purple-500 to-pink-500 flex flex-col rounded-lg p-2 items-center justify-center hover:shadow-lg hover:from-purple-600 hover:to-pink-600 transition-all duration-300"
             >
               <div className="flex">
-                <BotMessageSquare size={30} className="m-1 text-purple-600" />
-                <MessageSquare size={30} className="m-1 text-purple-600" />
+                <BotMessageSquare size={30} className="m-1 text-white" />
+                <MessageSquare size={30} className="m-1 text-white" />
               </div>
-
-              <p className=" text-xs">Vanlig LLM chat</p>
+              <p className="text-xs text-white">Vanlig LLM chat</p>
             </Link>
           </div>
         </div>
