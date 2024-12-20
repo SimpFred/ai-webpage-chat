b59283151dff54dcc6322bd3770233cec255340c
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-12-20 14:34:09 +0100

    Update ChatInput component styling for improved layout and responsiveness

--- app/components/chatInput.tsx
+++ app/components/chatInput.tsx
@@ -24,11 +24,11 @@ export const ChatInput = ({
   isAnswering,
 }: ChatInputProps) => {
   return (
-    <div className="z-10 bg-zinc-900 absolute bottom-0 left-0 w-full">
-      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
-        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
-          <div className="relative flex flex-col w-full flex-grow p-4">
-            <form onSubmit={handleSubmit} className="relative w-[60%] mx-auto">
+    <div className="z-10 bg-zinc-800 absolute border-none bottom-0 left-0 w-full">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl ">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col ">
+          <div className="relative flex flex-col w-full flex-grow pb-2">
+            <form onSubmit={handleSubmit} className="relative w-[80%] mx-auto">
               <Textarea
                 minRows={4}
                 variant="faded"
