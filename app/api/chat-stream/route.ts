7a40b0323df9a36bba602d4061313bb04e1eb0c1
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2025-01-07 14:28:40 +0100

    Add namespace retrieval from Redis in chat stream API

--- app/api/chat-stream/route.ts
+++ app/api/chat-stream/route.ts
@@ -1,15 +1,26 @@
 import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
 import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest, NextResponse } from "next/server";
+interface NamespaceData {
+  namespace: string;
+}
 
 export const POST = async (req: NextRequest) => {
   try {
     const { messages, sessionId } = await req.json();
     const lastMessage = messages[messages.length - 1].content;
+    const [encodedUrl] = sessionId.split("--");
+    const namespace: NamespaceData | null = await redis.hget(
+      "chat_namespace",
+      encodedUrl
+    );
+    console.log("Namespace:", namespace?.namespace);
 
     const response = await ragChat.chat(lastMessage, {
       streaming: true,
       sessionId,
+      namespace: namespace?.namespace,
     });
 
     return aiUseChatAdapter(response);
