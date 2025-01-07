2dd703f7852418cd8c9b9fcef4c126285aaa48ba
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2025-01-07 14:29:52 +0100

    Add session metadata storage and update logic in chat session API

--- app/api/llm-stream/route.ts
+++ app/api/llm-stream/route.ts
@@ -3,6 +3,11 @@ import { redis } from "@/lib/redis";
 import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest, NextResponse } from "next/server";
 
+interface SessionData {
+  title: string;
+  lastModified: number;
+}
+
 export const POST = async (req: NextRequest) => {
   try {
     const { messages, sessionId } = await req.json();
@@ -13,11 +18,15 @@ export const POST = async (req: NextRequest) => {
     }
 
     // Kontrollera om sessionId redan finns
-    const existingTitle = await redis.hget("chat_sessions", sessionId);
-    if (!existingTitle) {
+    const existingSession: SessionData | null = await redis.hget(
+      "chat_sessions",
+      sessionId
+    );
+    console.log("Existing session:", existingSession);
+    if (!existingSession) {
       // Skapa en titel via RAG
       const titleResponse = await ragChat.chat(
-        `Based on the context of this message, please come up with a suitable title for this chat. Only return the title and nothing else. Message: "${lastMessage}"`,
+        `Based on the context of this message, please come up with a suitable title for this chat. Only return the title and nothing else not even '"'. Message: "${lastMessage}"`,
         {
           sessionId,
           disableRAG: true,
@@ -25,9 +34,24 @@ export const POST = async (req: NextRequest) => {
       );
 
       const title = titleResponse.output.trim();
+      const lastModified = Date.now();
 
-      // Spara sessionId och titel i Redis-hashen
-      await redis.hset("chat_sessions", { [sessionId]: title });
+      // Spara sessionId, titel och lastModified i Redis-hashen
+      await redis.hset("chat_sessions", {
+        [sessionId]: JSON.stringify({
+          title: title,
+          lastModified: lastModified,
+        }),
+      });
+      // Radera alla nuvarande meddelanden i sessionen (titel frågan och svaret) för att inte spara den i chat historiken
+      ragChat.history.deleteMessages({ sessionId });
+    } else {
+      existingSession.lastModified = Date.now();
+
+      // Spara den uppdaterade sessionen
+      await redis.hset("chat_sessions", {
+        [sessionId]: JSON.stringify(existingSession),
+      });
     }
 
     // Skapa eller uppdatera chat-sessionen
