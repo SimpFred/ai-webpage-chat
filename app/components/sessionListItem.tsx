78c327a09a388b547b69e6e8f8ce4d8d2eecd40e
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2025-01-08 17:03:57 +0100

    Add ellipsis icon to session list item for improved UI interaction

--- app/components/sessionListItem.tsx
+++ app/components/sessionListItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { setCookie } from "cookies-next/client";
+import { Ellipsis } from "lucide-react";
 
 interface SessionListItemProps {
   sessionId: string;
@@ -14,9 +15,12 @@ const SessionListItem = ({ sessionId, title }: SessionListItemProps) => {
 
   return (
     <li className="mb-4">
-      <div onClick={handleClick}>
-        <div className="text-white text-xs truncate hover:underline">
+      <div onClick={handleClick} className="flex items-center">
+        <div className="flex-grow mr-2 text-white text-xs truncate hover:underline cursor-pointer">
           {title}
+        </div>
+        <div className="flex-shrink-0">
+          <Ellipsis size={15} className="text-white" />
         </div>
       </div>
     </li>
